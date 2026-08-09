16d137321322f76354d1dc739089a8be74740e34
Kiryl Kurzau <jackertfun228@mail.ru>
2026-08-09 11:23:53 +0200

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(ktorLibs.plugins.ktor)
+}
+
+group = "com.telegram"
+version = "1.0.0-SNAPSHOT"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+dependencies {
+    implementation(ktorLibs.server.config.yaml)
+    implementation(ktorLibs.server.core)
+    implementation(ktorLibs.server.cors)
+    implementation(ktorLibs.server.netty)
+    implementation(ktorLibs.server.routingOpenapi)
+    implementation(ktorLibs.server.swagger)
+    implementation(libs.logback.classic)
+
+    testImplementation(kotlin("test"))
+    testImplementation(ktorLibs.server.testHost)
+}
