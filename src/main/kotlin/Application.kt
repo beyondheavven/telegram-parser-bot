2d370f228f635978ae06c246f09fc7a1f977b5bd
Kiryl Kurzau <jackertfun228@mail.ru>
2026-08-09 14:07:07 +0200

    included configure modules in main

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,5 +1,16 @@
 package com.telegram
 
+import com.telegram.plugins.configureHttp
+import com.telegram.plugins.configureRouting
+import com.telegram.plugins.configureTdLight
+import io.ktor.server.application.Application
+
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureTdLight()
+    configureRouting()
+    configureHttp()
 }
